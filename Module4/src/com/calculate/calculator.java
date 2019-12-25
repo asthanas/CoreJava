d109ff43cac115c99eaceb4da0463e17454e6016
saurabh asthana <crypticelegance@gmail.com>
2019-12-25 03:08:41 -0500

    commit

--- Module4/src/com/calculate/calculator.java
+++ Module4/src/com/calculate/calculator.java
@@ -14,19 +14,16 @@ public class calculator {
 		int x = sc.nextInt();
 		int y = sc.nextInt();
 		
-		int reverse = cf.reverse(x);
-		int fact = cf.factor(x);
 		int sub = cf.subtract(x, y);
 		int mul = cf.multiply(x, y);
 		double div = cf.devide(x, y);
-		
-		System.out.println("Factorial of "+ x + " is "+ fact);
-		System.out.println("Reverse of "+ x + " is "+ reverse); 
+		int reverse = cf.reverse(x);
+		int fact = cf.factor(x);
+
 		System.out.println("Subtraction result is "+ sub); 
 		System.out.println("Multiplicaton result is "+ mul); 
 		System.out.println("Division result is "+ div); 
-
-
+		System.out.println("Factorial of "+ x + " is "+ fact);
+		System.out.println("Reverse of "+ x + " is "+ reverse); 
 	}
-
 }
